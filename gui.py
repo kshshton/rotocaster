ac83dce277f152aeb946a6bb1c539c68449b35c5
kshshton <michkrzyszton@gmail.com>
2024-12-08 17:00:43 +0100

    Add logic to buttons

--- gui.py
+++ gui.py
@@ -5,9 +5,11 @@ from customtkinter import CTk
 class GUI(CTk):
     def __init__(self) -> None:
         super().__init__()
-        self.geometry("300x200")
+        self.geometry("300x250")
         self.title("RotoCaster")
+        self.__button_padding = {"pady":10, "padx": 10}
         self.__center_window(self)
+        self.__default_text = "Wybierz profil"
         self.__profiles = {}
         self.__active_profile = None
         self.__speed = ctk.IntVar()
@@ -35,13 +37,27 @@ class GUI(CTk):
         print(self.__profiles)
 
     def __get_profile_value(self) -> int:
+        print(self.__profiles[self.__active_profile])
         return self.__profiles[self.__active_profile]
 
     def __save_profile(self, name: str) -> None:        
         self.__profiles[name] = 0
 
     def __select_profile(self, name: str) -> None:
-        self.__active_profile = name        
+        self.__active_profile = name
+
+    def __add_profile(self) -> None:
+        value = self.combobox.get()
+        self.__active_profile = value
+        self.__save_profile(self.__active_profile)
+        self.combobox.configure(values=list(self.__profiles.keys()))
+        self.__edit_profile()
+
+    def __delete_profile(self) -> None:    
+        value = self.combobox.get()
+        del self.__profiles[value]
+        self.combobox.configure(values=list(self.__profiles.keys()))
+        self.combobox.set(self.__default_text)
 
     def __edit_profile(self) -> None:
         edit_window = ctk.CTkToplevel()
@@ -73,39 +89,25 @@ class GUI(CTk):
     def __close_window(self, window: ctk.CTkToplevel) -> None:
         window.destroy()
 
-    def __delete_profile(self, name: str) -> None:    
-        del self.__profiles[name]
-        self.combobox.configure(values=list(self.__profiles.keys()))    
+    def __add_button(self, text: str, func: callable):
+        button = ctk.CTkButton(
+            master=self,
+            text=text,
+            command=func,
+        )
+        button.pack(**self.__button_padding, anchor='center')
 
     def __window(self) -> None:
         self.combobox = ctk.CTkComboBox(
             master=self,
             values=list(self.__profiles.keys()),
-            command=self.__select_profile,
-            
+            command=self.__select_profile            
         )
-        self.combobox.pack(pady=10, padx=10, anchor='center')
-        self.combobox.set("Wybierz profil")
-
-        self.add_button = ctk.CTkButton(
-            master=self,
-            text="Dodaj",
-            command=self.__edit_profile,
-        )
-        self.add_button.pack(pady=10, padx=10, anchor='center')
-
-        self.edit_button = ctk.CTkButton(
-            master=self,
-            text="Edytuj",
-            command=self.__edit_profile,
-        )
-        self.edit_button.pack(pady=10, padx=10, anchor='center')
-
-        self.execute_button = ctk.CTkButton(
-            master=self,
-            text="Uruchom",
-            command=self.__get_profile_value,
-        )
-        self.execute_button.pack(pady=10, padx=10, anchor='center')
+        self.combobox.pack(**self.__button_padding, anchor='center')
+        self.combobox.set(self.__default_text)
+        self.__add_button("Dodaj", self.__add_profile)
+        self.__add_button("Usuń", self.__delete_profile)
+        self.__add_button("Edytuj", self.__edit_profile)
+        self.__add_button("Uruchom", self.__get_profile_value)
 
         self.mainloop()
