4c7901c526491324e108bd39e25614f4b3338744
kshshton <michkrzyszton@gmail.com>
2024-12-17 22:47:50 +0100

    Fix indent in custom_button.py

--- custom_components/custom_button.py
+++ custom_components/custom_button.py
@@ -3,12 +3,12 @@ from customtkinter import CTk, CTkButton
 
 class CustomButton(CTkButton):
     def __init__(
-            self,
-            master: CTk, 
-            text: str, 
-            callback: callable,
-            anchor: str = "center",
-            padding: dict = {"pady": 10, "padx": 10},
+        self,
+        master: CTk, 
+        text: str, 
+        callback: callable,
+        anchor: str = "center",
+        padding: dict = {"pady": 10, "padx": 10},
     ) -> None:
         super().__init__(
             master=master,
