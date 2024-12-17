5edf565ec24e70be1111938ff42eab7ca7e7ba1b
kshshton <michkrzyszton@gmail.com>
2024-12-17 22:46:17 +0100

    Add refresh rate

--- speed/speed_damper.py
+++ speed/speed_damper.py
@@ -6,8 +6,9 @@ from speed.speed_operator import SpeedOperator
 
 
 class SpeedDamper(CTk):
-    def __init__(self) -> None:
+    def __init__(self, refresh_rate: int = 100) -> None:
         super().__init__()
+        self.__refresh_rate = refresh_rate
         self.actual_speed: int = 0
         self.current_profile_speed: int = 0
 
@@ -15,17 +16,17 @@ class SpeedDamper(CTk):
         if self.actual_speed == 0:
             return self.actual_speed
         self.actual_speed -= 1
-        self.after(ms=100, func=self.__reset_speed_value)
+        self.after(ms=self.__refresh_rate, func=self.__reset_speed_value)
 
     def __reach_current_profile_speed(self) -> None:
         if self.actual_speed == self.current_profile_speed:
             return self.actual_speed
         self.actual_speed += 1
-        self.after(ms=100, func=self.__reach_current_profile_speed)
+        self.after(ms=self.__refresh_rate, func=self.__reach_current_profile_speed)
 
     def _listen_output_speed(self) -> None:
         print(self.actual_speed)
-        self.after(ms=100, func=self._listen_output_speed)
+        self.after(ms=self.__refresh_rate, func=self._listen_output_speed)
 
     def speed_operation(self, operator: SpeedOperator) -> None:
         match operator:
